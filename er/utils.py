726647ee0a7a07be4c69465c1b4903d240f3566e
dalpozz <dalpozz@gmail.com>
2025-01-28 20:33:42 +0100

    fix poetry

--- er/utils.py
+++ er/utils.py
@@ -20,12 +20,7 @@ def read_yaml_file(filename):
 
 
 def get_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+    """Grab data."""
 
     DATA_FILENAME = Path(__file__).parent / "config.yml"
     df = read_yaml_file(DATA_FILENAME)
